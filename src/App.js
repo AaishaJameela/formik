5a8b8447518271254b16a239b9a336854d0ba764
AaishaJameela <aaishjami@gmail.com>
2022-09-02 17:35:16 +0530

    package-lock.json package.json Added error messages: src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <div>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validate={(values) => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = "Email is required";
+        } else if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(values.email)) {
+          errors.email = "Invalid Email";
+        }
+        if (!values.password) {
+          errors.password = "Password is required";
+        } else if (values.password.length > 8) {
+          errors.password = "Password length cannot be more than 8 characters";
+        } else if (values.password.length < 4) {
+          errors.password = "Password length cannot be less than 4 characters";
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <center>
+        <h1>Sign Up</h1>{" "}
+        <Form>
+          <label>Email:</label>
+          <br />
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <br />
+          <ErrorMessage name="email" component="div" />
+          <label>Password:</label>
+          <br />
+          <Field type="password" id="password" name="password" />
+          <br />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit">Submit</button>
+        </Form>
+      </center>
+    </Formik>
+  </div>
+);
 export default App;
